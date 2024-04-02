6facbcbf20a3377c79152fa967b8531e07f17ecd
AbdumajidovAvazbek <abdumajidova51@gmail.com>
2024-04-02 10:19:57 +0500

    Added: wrote Category service

---
+++ src/KitobNur.Service/Extentions/PagedList.cs
@@ -0,0 +1,18 @@
+﻿using KitobNur.Service.Configurations;
+
+namespace KitobNur.Service.Extensions
+{
+    public class PagedList<T>
+    {
+        public List<T> Items { get; set; }
+        public int TotalCount { get; set; }
+        public PaginationMetaData MetaData { get; set; }
+
+        public PagedList(List<T> items, int totalCount, PaginationParams @params)
+        {
+            Items = items;
+            TotalCount = totalCount;
+            MetaData = new PaginationMetaData(totalCount, @params);
+        }
+    }
+}
